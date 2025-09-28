239dee64044014dcd4d361698f0c78a39e6fde06
Ranjit D. Pawar <164663045+proggramingPro@users.noreply.github.com>
2025-09-28 21:57:21 +0530

    Update page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -17,7 +17,7 @@ export default function LoginPage() {
     const loginData = { email, password };
 
     try {
-      const res = await fetch("https://suply-chain-l1jg.vercel.app//api/auth/login", {
+      const res = await fetch("https://suplychain.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(loginData),
