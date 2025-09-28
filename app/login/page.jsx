1983be5c5ce9765d42b75b79072df11819e538ae
Ranjit D. Pawar <164663045+proggramingPro@users.noreply.github.com>
2025-09-28 21:43:30 +0530

    Update page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -17,7 +17,7 @@ export default function LoginPage() {
     const loginData = { email, password };
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("https://vercel.com/ranjit084s-projects/suply-chain-l1jg/2R1ovtfpkiFxfnsKNW8kCC5WQKou/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(loginData),
