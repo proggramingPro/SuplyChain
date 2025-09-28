ef13c1a961f1ba6c56605cd5028b1a9ed6386f6e
Ranjit D. Pawar <164663045+proggramingPro@users.noreply.github.com>
2025-09-28 21:58:33 +0530

    Update page.jsx

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -56,7 +56,7 @@ export default function SignupPage() {
     setIsLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/verify-otp", {
+      const res = await fetch("https://suplychain.onrender.com/api/auth/verify-otp", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, otp }),
